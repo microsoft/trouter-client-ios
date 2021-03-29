0e941081bb8d1bbcd79b2bc5d1812c96126ff054
Travis Prescott <tjprescott@users.noreply.github.com>
2021-03-29 09:49:29 -0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version:5.3
+//  The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// --------------------------------------------------------------------------
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// The MIT License (MIT)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the ""Software""), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+// --------------------------------------------------------------------------
+import PackageDescription
+
+let package = Package(
+    name: "Trouter",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v12), .tvOS(.v12)
+    ],
+    products: [
+        .library(name: "Trouter", targets: ["Trouter"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Build targets
+        .binaryTarget(
+          name: "Trouter",
+          url: "https://github.com/microsoft/trouter-client-ios/releases/download/v0.0.1-beta.3/TrouterClientIos-0.0.1-beta.3.zip",
+          checksum: "34651d8be50e86519d31a0f5a644b925fcc0e7e00c8424a29c2e96b8283f797b"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
