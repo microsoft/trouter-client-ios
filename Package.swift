d617a410a35cd2bbea514b243bde57e4f9d60fdb
Skype BuildSystems Build Agent <sbuilder@microsoft.com>
2021-03-29 23:24:22 +0200

    Version v0.0.1-beta.4

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+import PackageDescription
+
+let package = Package(
+    name: "TrouterClientIos",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "TrouterClientIos", targets: ["TrouterClientIos"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+          name: "TrouterClientIos",
+          url: "https://github.com/microsoft/trouter-client-ios/releases/download/v0.0.1-beta.4/TrouterClientIos-0.0.1-beta.4.zip",
+          checksum: "5cb5737549ebcf87a3db367737b8c0a5806f6b36d17e60936ac3ee283292ad3b"
+        ),
+    ],
+    swiftLanguageVersions: [.v5],
+)
